33b9dd7055f36250a8168de65f292271232c18d2
heroxzol <lkokosko@mail.ru>
2026-06-03 04:29:02 -0700

    fix middleware

--- backend/coffee_shop/settings.py
+++ backend/coffee_shop/settings.py
@@ -36,10 +36,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    
-
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
